8a8ca868053c5ee0ba5b426e7b95b08b3987b6e0
Rei Sikk <rei@hable.ee>
2025-10-15 13:33:10 +0300

    Buy me a coffee button added to footer, footer styling

--- src/app/components/SiteFooter.tsx
+++ src/app/components/SiteFooter.tsx
@@ -4,15 +4,27 @@ function SiteFooter() {
   return (
     <footer className="siteFooter container">
         <div className="sitefooter__main fp">
-        <h5 className="siteFooter__title h1 uppercase italic">Tln Run Clubs</h5>
+          <h5 className="siteFooter__title h1 uppercase italic">Run Clubs Est</h5>
+          <a href="https://www.reihopsti.ee" target="_blank" rel="noopener noreferrer">Idea, design & code by <span>Rei Sikk</span></a>
+          <span>All rights reserved © {new Date().getFullYear()}</span>
         </div>
         <div className="siteFooter__side fp">
+        <a
+          className="siteFooter__bmac btn_main"
+          target="_blank"
+          href="https://www.buymeacoffee.com/tlnrunclubs"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="coffeeImage"
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            alt="Buy me a coffee"
+          />
+          <span className="txt-body">Buy me a coffee</span>
+        </a>
         <p className="siteFooter__text">
-            Buy me a coffee.
-            This helps cover hosting & domain costs. Thank you 🙏
+            This helps cover hosting & domain costs.<br /> Thank you 🙏
         </p>
-        <a href="https://www.reihopsti.ee" target="_blank" rel="noopener noreferrer">Idea, design & code by <span>Rei Sikk</span></a>
-        <span>All rights reserved © {new Date().getFullYear()}</span>
         </div>
     </footer>
   )
